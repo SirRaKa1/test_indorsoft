cbfd19c1c8bc048cbea677d53a73620b9e00a43e
RaKaLuS <sir.d-i-a2014@mail.ru>
2024-03-18 17:05:44 +0700

    init commit

---
+++ src/main/java/com/company/test_indorsoft/view/user/UserListView.java
@@ -0,0 +1,14 @@
+package com.company.test_indorsoft.view.user;
+
+import com.company.test_indorsoft.entity.User;
+import com.company.test_indorsoft.view.main.MainView;
+import com.vaadin.flow.router.Route;
+import io.jmix.flowui.view.*;
+
+@Route(value = "users", layout = MainView.class)
+@ViewController("User.list")
+@ViewDescriptor("user-list-view.xml")
+@LookupComponent("usersDataGrid")
+@DialogMode(width = "64em")
+public class UserListView extends StandardListView<User> {
+}
